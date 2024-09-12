57f82c4a7d5728078b66fd678e2326fb5a6aebf9
riquelmmy <riquelmmy112@gmail.com>
2024-09-12 18:12:52 -0300

    subindo

--- __tests__/endpoints/users/UserController.test.ts
+++ __tests__/endpoints/users/UserController.test.ts
@@ -3,6 +3,7 @@ import { App } from '../../../src/app';
 import { IUser } from '../../../src/interfaces/IUser';
 import { IUserResponse } from '../../../src/interfaces/IUserResponse';
 import { UserRepository } from '../../../src/endpoints/users/userRepository';
+import { response } from 'express';
 
 // Cria uma instância da aplicação para executar os testes
 const app = new App().server.listen(8081);
@@ -60,4 +61,52 @@ describe('UserController', () => {
     expect(response.body.success).toBe(true);
     expect(response.body.data).toEqual(expectedUsers);
   });
+
+  it('Deve retornar uma lista vazia', async () =>{
+    const mockUsers: IUser[] = []
+    const expectedUsers: IUserResponse[] = []
+
+    jest.spyOn(UserRepository.prototype, 'list').mockReturnValueOnce(mockUsers);
+
+    const response = await request(app).get('/users');
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.data).toEqual(expectedUsers);
+  })
+
+  it('Deve retornar um usuário por id', async () => {
+    const mockUser: IUser = {
+      id: 1,
+      name: 'Chouji',
+      age: 10,
+    };
+  
+    const expectedUser: IUserResponse = {
+      id: 1,
+      name: 'Chouji',
+      age: 10,
+      isOfAge: false,
+    };
+    jest.spyOn(UserRepository.prototype, 'findOne').mockReturnValue(mockUser);
+    const response = await request(app).get('/users/id')
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+    success: true,
+    data: expectedUser,
+  });
+  })
+  
+  it("criar um usuário", async () =>{
+    const mockUser : IUser = {
+      id: 4,
+      name: 'Orochi',
+      age: 50,
+    };
+
+    jest.spyOn(UserRepository.prototype, 'save').mockReturnValueOnce(!!mockUser)
+    const response = await request(app).post('/users');
+    expect(response.status).toBe(201);
+
+  })
+
 });
